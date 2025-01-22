0b27d0cd49cf546f4f152cd4eed4c76b0edf3df8
Mauro Medda <medda.mauro@gmail.com>
2025-01-22 15:56:44 +0100

    feat(): replace geth w/ reth

--- clean.sh
+++ clean.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 rm -Rf ./consensus/beacondata ./consensus/validatordata ./consensus/genesis.ssz
-bash geth-remove-db.sh
+rm -Rf ./execution/geth
+rm -Rf ./execution/geth.ipc
+rm -Rf ./execution/retrh ./execution/blobstore/ ./execution/invalid_block_hooks/ ./execution/db ./execution/static_files/
+rm -rf 	./execution/discovery-secret \
+	./execution/known-peers.json \
+	./execution/reth.toml
--- geth-remove-db.sh
+++
@@ -1,4 +0,0 @@
-# Removes the database of the go-ethereum execution client to ensure we start from a clean state.
-# (geth has a `removedb` option, but it asks for a keyboard confirmation, so we use this instead)
-rm -Rf ./execution/geth
-rm -Rf ./execution/geth.ipc
